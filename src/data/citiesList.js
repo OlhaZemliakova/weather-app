2b04838b85015ecd48ead125a347dfe583411520
Olha Zemliakova <zemlakova@gmail.com>
2023-01-13 16:12:34 -0800

    Created citiesList.js

---
+++ src/data/citiesList.js
@@ -0,0 +1,62 @@
+export const cities = [
+  {
+    name: "Vancouver",
+    latitude: 49.25,
+    longitude: -123.12,
+    default: true,
+  },
+  {
+    name: "Montreal",
+    latitude: 45.51,
+    longitude: -73.59,
+    default: false,
+  },
+  {
+    name: "Paris",
+    latitude: 48.85,
+    longitude: 2.35,
+    default: false,
+  },
+  {
+    name: "Berlin",
+    latitude: 52.52,
+    longitude: 13.41,
+    default: false,
+  },
+  {
+    name: "Szczecin",
+    latitude: 53.43,
+    longitude: 14.55,
+    default: false,
+  },
+  {
+    name: "Valletta",
+    latitude: 35.9,
+    longitude: 14.51,
+    default: false,
+  },
+  {
+    name: "Stone Town",
+    latitude: -6.16,
+    longitude: 39.19,
+    default: false,
+  },
+  {
+    name: "Khmelnytskyy",
+    latitude: 49.42,
+    longitude: 26.98,
+    default: false,
+  },
+  {
+    name: "Dubai City",
+    latitude: 25.08,
+    longitude: 55.31,
+    default: false,
+  },
+  {
+    name: "Sharm el Sheikh",
+    latitude: 27.92,
+    longitude: 34.33,
+    default: false,
+  },
+];
